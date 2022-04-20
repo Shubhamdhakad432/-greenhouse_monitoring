28b397f50fb08a0b532d1aa4488bfd9a8595fcc2
SHUBHAM DHAKAD <shubhamdhakad432@gmail.com>
2022-04-20 23:06:38 +0530

    update graph color or change bar chart to line chart

--- app/src/main/java/com/example/greenhousemonitoring/SensorData.java
+++ app/src/main/java/com/example/greenhousemonitoring/SensorData.java
@@ -21,9 +21,15 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import org.json.JSONArray;
@@ -64,9 +70,9 @@ public class SensorData extends AppCompatActivity {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    BarChart barChart_temp;
-                    BarData barData_temp;
-                    BarDataSet barDataSet_temp;
+                    LineChart barChart_temp;
+                    LineData barData_temp;
+                    LineDataSet barDataSet_temp;
                     JSONArray result = response.getJSONArray("results");
                     JSONObject json_array_0 = result.getJSONObject(0);
                     double value0 = json_array_0.getDouble("value");
@@ -85,22 +91,24 @@ public class SensorData extends AppCompatActivity {
                     JSONObject json_array_7 = result.getJSONObject(7);
                     double value7 = json_array_7.getDouble("value");
                     barChart_temp=findViewById(R.id.temp_BarChart);
-                    List<BarEntry> barEntries_temp = new ArrayList<>();
-                    barEntries_temp.add(new BarEntry(1,(float)value7));
-                    barEntries_temp.add(new BarEntry(2,(float)value6));
-                    barEntries_temp.add(new BarEntry(3,(float)value5));
-                    barEntries_temp.add(new BarEntry(4,(float)value4));
-                    barEntries_temp.add(new BarEntry(5,(float)value3));
-                    barEntries_temp.add(new BarEntry(6,(float)value2));
-                    barEntries_temp.add(new BarEntry(7,(float)value1));
-                    barEntries_temp.add(new BarEntry(8,(float)value0));
+                    List<Entry> barEntries_temp = new ArrayList<>();
+                    barEntries_temp.add(new Entry(0.9999999999f,0f));
+                    barEntries_temp.add(new Entry(1,(float)value7));
+                    barEntries_temp.add(new Entry(2,(float)value6));
+                    barEntries_temp.add(new Entry(3,(float)value5));
+                    barEntries_temp.add(new Entry(4,(float)value4));
+                    barEntries_temp.add(new Entry(5,(float)value3));
+                    barEntries_temp.add(new Entry(6,(float)value2));
+                    barEntries_temp.add(new Entry(7,(float)value1));
+                    barEntries_temp.add(new Entry(8,(float)value0));
 
-                    barDataSet_temp=new BarDataSet(barEntries_temp,"Temperature");
-                    barData_temp=new BarData(barDataSet_temp);
+                    barDataSet_temp=new LineDataSet(barEntries_temp,"Temperature");
+                    barData_temp=new LineData(barDataSet_temp);
                     barChart_temp.setData(barData_temp);
-                    barDataSet_temp.setColors(ColorTemplate.JOYFUL_COLORS);
+//                    barDataSet_temp.setColors(ColorTemplate.JOYFUL_COLORS);
+                    barDataSet_temp.setColors(ColorTemplate.rgb("#179D9A"));
                     barDataSet_temp.setValueTextColor(Color.BLACK);
-                    barDataSet_temp.setValueTextSize(5f);
+                    barDataSet_temp.setValueTextSize(8f);
 
                     TextView textView = (TextView) findViewById(R.id.temp_display);
                     textView.setText(value0 + " ℃");
@@ -133,9 +141,9 @@ public class SensorData extends AppCompatActivity {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    BarChart barChart_humid;
-                    BarData barData_humid;
-                    BarDataSet barDataSet_humid;
+                    LineChart barChart_humid;
+                    LineData barData_humid;
+                    LineDataSet barDataSet_humid;
                     JSONArray result = response.getJSONArray("results");
                     JSONObject json_array_0 = result.getJSONObject(0);
                     double value0 = json_array_0.getDouble("value");
@@ -154,21 +162,22 @@ public class SensorData extends AppCompatActivity {
                     JSONObject json_array_7 = result.getJSONObject(7);
                     double value7 = json_array_7.getDouble("value");
                     barChart_humid=findViewById(R.id.humid_BarChart);
-                    List<BarEntry> barEntries_humid = new ArrayList<>();
-                    barEntries_humid.add(new BarEntry(1,(float)value7));
-                    barEntries_humid.add(new BarEntry(2,(float)value6));
-                    barEntries_humid.add(new BarEntry(3,(float)value5));
-                    barEntries_humid.add(new BarEntry(4,(float)value4));
-                    barEntries_humid.add(new BarEntry(5,(float)value3));
-                    barEntries_humid.add(new BarEntry(6,(float)value2));
-                    barEntries_humid.add(new BarEntry(7,(float)value1));
-                    barEntries_humid.add(new BarEntry(8,(float)value0));
-                    barDataSet_humid=new BarDataSet(barEntries_humid,"Humidity");
-                    barData_humid=new BarData(barDataSet_humid);
+                    List<Entry> barEntries_humid = new ArrayList<>();
+                    barEntries_humid.add(new Entry(0.9999999999f,0f));
+                    barEntries_humid.add(new Entry(1,(float)value7));
+                    barEntries_humid.add(new Entry(2,(float)value6));
+                    barEntries_humid.add(new Entry(3,(float)value5));
+                    barEntries_humid.add(new Entry(4,(float)value4));
+                    barEntries_humid.add(new Entry(5,(float)value3));
+                    barEntries_humid.add(new Entry(6,(float)value2));
+                    barEntries_humid.add(new Entry(7,(float)value1));
+                    barEntries_humid.add(new Entry(8,(float)value0));
+                    barDataSet_humid=new LineDataSet(barEntries_humid,"Humidity");
+                    barData_humid=new LineData(barDataSet_humid);
                     barChart_humid.setData(barData_humid);
-                    barDataSet_humid.setColors(ColorTemplate.JOYFUL_COLORS);
+                    barDataSet_humid.setColors(ColorTemplate.rgb("#179D9A"));
                     barDataSet_humid.setValueTextColor(Color.BLACK);
-                    barDataSet_humid.setValueTextSize(5f);
+                    barDataSet_humid.setValueTextSize(8f);
 
                     TextView textView = (TextView) findViewById(R.id.hum_display);
                     textView.setText(value0 + " %RH");
@@ -197,116 +206,53 @@ public class SensorData extends AppCompatActivity {
 
 
         // light bar
-        BarChart barChart_light;
-        BarData barData_light;
-        BarDataSet barDataSet_light;
-
-//        JSONArray result = response.getJSONArray("results");
-//        JSONObject json_array_0 = result.getJSONObject(0);
-//        double value0 = json_array_0.getDouble("value");
-//        JSONObject json_array_1 = result.getJSONObject(1);
-//        double value1 = json_array_1.getDouble("value");
-//        JSONObject json_array_2 = result.getJSONObject(2);
-//        double value2 = json_array_2.getDouble("value");
-//        JSONObject json_array_3 = result.getJSONObject(3);
-//        double value3 = json_array_3.getDouble("value");
-//        JSONObject json_array_4 = result.getJSONObject(4);
-//        double value4 = json_array_4.getDouble("value");
-//        JSONObject json_array_5 = result.getJSONObject(5);
-//        double value5 = json_array_5.getDouble("value");
-//        JSONObject json_array_6 = result.getJSONObject(6);
-//        double value6 = json_array_6.getDouble("value");
-//        JSONObject json_array_7 = result.getJSONObject(7);
-//        double value7 = json_array_7.getDouble("value");
-//        barChart_humid=findViewById(R.id.light_BarChart);
-//        List<BarEntry> barEntries_light = new ArrayList<>();
-//        barEntries_light.add(new BarEntry(1,(float)value7));
-//        barEntries_light.add(new BarEntry(2,(float)value6));
-//        barEntries_light.add(new BarEntry(3,(float)value5));
-//        barEntries_light.add(new BarEntry(4,(float)value4));
-//        barEntries_light.add(new BarEntry(5,(float)value3));
-//        barEntries_light.add(new BarEntry(6,(float)value2));
-//        barEntries_light.add(new BarEntry(7,(float)value1));
-//        barEntries_light.add(new BarEntry(8,(float)value0));
-//        barDataSet_light=new BarDataSet(barEntries_light,"Light");
-//        barData_light=new BarData(barDataSet_light);
-//        barChart_light.setData(barData_light);
-//        barDataSet_light.setColors(ColorTemplate.JOYFUL_COLORS);
-//        barDataSet_light.setValueTextColor(Color.BLACK);
-//        barDataSet_light.setValueTextSize(5f);
+        LineChart barChart_light;
+        LineData barData_light;
+        LineDataSet barDataSet_light;
 
         barChart_light=findViewById(R.id.light_BarChart);
-        List<BarEntry> barEntries_light = new ArrayList<>();
-        barEntries_light.add(new BarEntry(1,501f));
-        barEntries_light.add(new BarEntry(2,504.2f));
-        barEntries_light.add(new BarEntry(3,508f));
-        barEntries_light.add(new BarEntry(4,499.2f));
-        barEntries_light.add(new BarEntry(5,505.5f));
-        barEntries_light.add(new BarEntry(6,500f));
-        barEntries_light.add(new BarEntry(7,502.8f));
-        barEntries_light.add(new BarEntry(8,503.8f));
-        barDataSet_light=new BarDataSet(barEntries_light,"Light");
-        barData_light=new BarData(barDataSet_light);
+        List<Entry> barEntries_light = new ArrayList<>();
+        barEntries_light.add(new Entry(0.9999999999f,0f));
+        barEntries_light.add(new Entry(1,501f));
+        barEntries_light.add(new Entry(2,504.2f));
+        barEntries_light.add(new Entry(3,508f));
+        barEntries_light.add(new Entry(4,499.2f));
+        barEntries_light.add(new Entry(5,505.5f));
+        barEntries_light.add(new Entry(6,500f));
+        barEntries_light.add(new Entry(7,502.8f));
+        barEntries_light.add(new Entry(8,503.8f));
+        barDataSet_light=new LineDataSet(barEntries_light,"Light");
+        barData_light=new LineData(barDataSet_light);
         barChart_light.setData(barData_light);
         barDataSet_light.setColors(ColorTemplate.JOYFUL_COLORS);
         barDataSet_light.setValueTextColor(Color.BLACK);
-        barDataSet_light.setValueTextSize(5f);
+        barDataSet_light.setValueTextSize(8f);
+
+
+
 
         // soil bar
-        BarChart barChart_soil;
-        BarData barData_soil;
-        BarDataSet barDataSet_soil;
-
-//        JSONArray result = response.getJSONArray("results");
-//        JSONObject json_array_0 = result.getJSONObject(0);
-//        double value0 = json_array_0.getDouble("value");
-//        JSONObject json_array_1 = result.getJSONObject(1);
-//        double value1 = json_array_1.getDouble("value");
-//        JSONObject json_array_2 = result.getJSONObject(2);
-//        double value2 = json_array_2.getDouble("value");
-//        JSONObject json_array_3 = result.getJSONObject(3);
-//        double value3 = json_array_3.getDouble("value");
-//        JSONObject json_array_4 = result.getJSONObject(4);
-//        double value4 = json_array_4.getDouble("value");
-//        JSONObject json_array_5 = result.getJSONObject(5);
-//        double value5 = json_array_5.getDouble("value");
-//        JSONObject json_array_6 = result.getJSONObject(6);
-//        double value6 = json_array_6.getDouble("value");
-//        JSONObject json_array_7 = result.getJSONObject(7);
-//        double value7 = json_array_7.getDouble("value");
-//        barChart_soil=findViewById(R.id.soil_BarChart);
-//        List<BarEntry> barEntries_soil = new ArrayList<>();
-//        barEntries_soil.add(new BarEntry(1,(float)value7));
-//        barEntries_soil.add(new BarEntry(2,(float)value6));
-//        barEntries_soil.add(new BarEntry(3,(float)value5));
-//        barEntries_soil.add(new BarEntry(4,(float)value4));
-//        barEntries_soil.add(new BarEntry(5,(float)value3));
-//        barEntries_soil.add(new BarEntry(6,(float)value2));
-//        barEntries_soil.add(new BarEntry(7,(float)value1));
-//        barEntries_soil.add(new BarEntry(8,(float)value0));
-//        barDataSet_soil=new BarDataSet(barEntries_soil,"Light");
-//        barData_soil=new BarData(barDataSet_soil);
-//        barChart_soil.setData(barData_soil);
-//        barDataSet_soil.setColors(ColorTemplate.JOYFUL_COLORS);
-//        barDataSet_soil.setValueTextColor(Color.BLACK);
-//        barDataSet_soil.setValueTextSize(5f);
+        LineChart barChart_soil;
+        LineData barData_soil;
+        LineDataSet barDataSet_soil;
 
         barChart_soil=findViewById(R.id.soil_BarChart);
-        List<BarEntry> barEntries_soil = new ArrayList<>();
-        barEntries_soil.add(new BarEntry(1,33.5f));
-        barEntries_soil.add(new BarEntry(2,35f));
-        barEntries_soil.add(new BarEntry(3,32f));
-        barEntries_soil.add(new BarEntry(4,30.2f));
-        barEntries_soil.add(new BarEntry(5,45.5f));
-        barEntries_soil.add(new BarEntry(6,28f));
-        barEntries_soil.add(new BarEntry(7,31f));
-        barEntries_soil.add(new BarEntry(8,30f));
-        barDataSet_soil=new BarDataSet(barEntries_soil,"Soil moisture");
-        barData_soil=new BarData(barDataSet_soil);
+        List<Entry> barEntries_soil = new ArrayList<>();
+        barEntries_soil.add(new Entry(0f,0f));
+        barEntries_soil.add(new Entry(1,33.5f));
+        barEntries_soil.add(new Entry(2,35f));
+        barEntries_soil.add(new Entry(3,32f));
+        barEntries_soil.add(new Entry(4,30.2f));
+        barEntries_soil.add(new Entry(5,45.5f));
+        barEntries_soil.add(new Entry(6,28f));
+        barEntries_soil.add(new Entry(7,31f));
+        barEntries_soil.add(new Entry(8,30f));
+        barDataSet_soil=new LineDataSet(barEntries_soil,"Soil moisture");
+        barData_soil=new LineData(barDataSet_soil);
         barChart_soil.setData(barData_soil);
         barDataSet_soil.setColors(ColorTemplate.JOYFUL_COLORS);
         barDataSet_soil.setValueTextColor(Color.BLACK);
-        barDataSet_soil.setValueTextSize(5f);
+        barDataSet_soil.setValueTextSize(8f);
     }
 
     public void login_actuator(View view) {
